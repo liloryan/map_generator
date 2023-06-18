4bdd82b937930e0d6ee74c519b60e511d65120b2
liloryan <liryan@hmc.edu>
2023-06-17 22:04:37 -0700

    got rid of /n at the end of images and then downloaded

--- 00_downloadImages.py
+++ 00_downloadImages.py
@@ -2,6 +2,7 @@
 import requests
 from tqdm import tqdm
 from pathlib import Path
+from time import sleep
 # %%
 cfg_path = Path('cfg_files')
 data_path = Path('data')
@@ -12,6 +13,12 @@ with (cfg_path/'images.txt').open('r') as f:
 
 for i in tqdm(range(len(url_list))):
     url = url_list[i].strip()
+    file_name = data_path/(str(i) + url[-4:])
+
+    if file_name.exists():
+        continue
+    
+    sleep(1)
     r = requests.get(url)
-    with open((data_path/(str(i) + url[-4:])),'wb') as f:
+    with open(file_name,'wb') as f:
         f.write(r.content)
