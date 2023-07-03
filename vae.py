3bdcc47e9593c31d2f7006e1da45ba33d508610b
liloryan <liryan@hmc.edu>
2023-07-02 20:21:03 -0700

    not working; vae code

--- vae.py
+++ vae.py
@@ -11,11 +11,15 @@ from torch import nn
 import torch.nn.functional as F
 import torch.optim as optim
 from pathlib import Path
+from torch.autograd import Variable
 #%%
 data_dir = Path('data')
+image_size = 224
+
 batch_size = 32
 epochs = 50
-image_size = 256
+lr = 1e-3
+
 torch.manual_seed(42)
 torch.cuda.manual_seed(42)
 device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -30,7 +34,8 @@ train_transform = transforms.Compose(
         transforms.Resize(size = image_size),
         transforms.RandomCrop(size = (image_size,image_size)),
         transforms.Grayscale(),
-        transforms.ToTensor()
+        transforms.ToTensor(),
+        transforms.Normalize((0.5), (0.5))
     ]
 )
 test_transform = transforms.Compose(
@@ -38,7 +43,8 @@ test_transform = transforms.Compose(
         transforms.Resize(size = image_size),
         transforms.CenterCrop(size = (image_size,image_size)),
         transforms.Grayscale(),
-        transforms.ToTensor()
+        transforms.ToTensor(),
+        transforms.Normalize((0.5), (0.5))
     ]
 )
 
@@ -50,7 +56,8 @@ test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size)
 #%%
 def imshow(img):
     """function to show image"""
-    npimg = img.numpy()  # convert to numpy objects
+    img = img / 2 + 0.5     # unnormalize
+    npimg = img.numpy()
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
     plt.show()
 
@@ -59,3 +66,149 @@ imshow(torchvision.utils.make_grid(images))
 
 
 # %%
+# modified from: https://medium.com/dataseries/variational-autoencoder-with-pytorch-2d359cbf027b
+
+class VariationalEncoder(nn.Module):
+    def __init__(self, latent_dims):  
+        super(VariationalEncoder, self).__init__()
+        self.conv1 = nn.Conv2d(1, 8, 3, stride=2, padding=1)
+        self.conv2 = nn.Conv2d(8, 16, 3, stride=2, padding=1)
+        self.batch2 = nn.BatchNorm2d(16)
+        self.conv3 = nn.Conv2d(16, 32, 3, stride=2, padding=0)  
+        self.linear1 = nn.Linear(3*3*32, 128)
+        self.linear2 = nn.Linear(128, latent_dims)
+        self.linear3 = nn.Linear(128, latent_dims)
+
+        self.N = torch.distributions.Normal(0, 1)
+        self.N.loc = self.N.loc.cuda() # hack to get sampling on the GPU
+        self.N.scale = self.N.scale.cuda()
+        self.kl = 0
+
+    def forward(self, x):
+        x = x.to(device)
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.batch2(self.conv2(x)))
+        x = F.relu(self.conv3(x))
+        x = torch.flatten(x, start_dim=1)
+        x = F.relu(self.linear1(x))
+        mu =  self.linear2(x)
+        sigma = torch.exp(self.linear3(x))
+        z = mu + sigma*self.N.sample(mu.shape)
+        self.kl = (sigma**2 + mu**2 - torch.log(sigma) - 1/2).sum()
+        return z      
+
+class Decoder(nn.Module):
+    
+    def __init__(self, latent_dims):
+        super().__init__()
+
+        self.decoder_lin = nn.Sequential(
+            nn.Linear(latent_dims, 128),
+            nn.ReLU(True),
+            nn.Linear(128, 3 * 3 * 32),
+            nn.ReLU(True)
+        )
+
+        self.unflatten = nn.Unflatten(dim=1, unflattened_size=(32, 3, 3))
+
+        self.decoder_conv = nn.Sequential(
+            nn.ConvTranspose2d(32, 16, 3, stride=2, output_padding=0),
+            nn.BatchNorm2d(16),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(16, 8, 3, stride=2, padding=1, output_padding=1),
+            nn.BatchNorm2d(8),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(8, 1, 3, stride=2, padding=1, output_padding=1)
+        )
+        
+    def forward(self, x):
+        x = self.decoder_lin(x)
+        x = self.unflatten(x)
+        x = self.decoder_conv(x)
+        x = torch.sigmoid(x)
+        return x
+
+class VariationalAutoencoder(nn.Module):
+    def __init__(self, latent_dims):
+        super(VariationalAutoencoder, self).__init__()
+        self.encoder = VariationalEncoder(latent_dims)
+        self.decoder = Decoder(latent_dims)
+
+    def forward(self, x):
+        x = x.to(device)
+        z = self.encoder(x)
+        return self.decoder(z)
+# %%
+vae = VariationalAutoencoder(latent_dims= 16)
+
+optim = torch.optim.Adam(vae.parameters(), lr=lr, weight_decay=1e-5)
+vae.to(device)
+#%%
+
+def train_epoch(vae, device, dataloader, optimizer):
+    # Set train mode for both the encoder and the decoder
+    vae.train()
+    trainloss = 0.0
+    # Iterate the dataloader (we do not need the label values, this is unsupervised learning)
+    for x,  in dataloader: 
+        # Move tensor to the proper device
+        x = x.to(device)
+        x_hat = vae(x)
+        # Evaluate loss
+        loss = ((x - x_hat)**2).sum() + vae.encoder.kl
+
+        # Backward pass
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        # Print batch loss
+        print('\t partial train loss (single batch): %f' % (loss.item()))
+        train_loss+=loss.item()
+
+    return train_loss / len(dataloader.dataset)
+def test_epoch(vae, device, dataloader):
+    # Set evaluation mode for encoder and decoder
+    vae.eval()
+    val_loss = 0.0
+    with torch.nograd(): # No need to track the gradients
+        for x,  in dataloader:
+            # Move tensor to the proper device
+            x = x.to(device)
+            # Encode data
+            encoded_data = vae.encoder(x)
+            # Decode data
+            x_hat = vae(x)
+            loss = ((x - x_hat)**2).sum() + vae.encoder.kl
+            val_loss += loss.item()
+
+    return val_loss / len(dataloader.dataset)
+def plot_ae_outputs(encoder,decoder,n=10):
+    plt.figure(figsize=(16,4.5))
+    targets = test_dataset.targets.numpy()
+    t_idx = {i:np.where(targets==i)[0][0] for i in range(n)}
+    for i in range(n):
+      ax = plt.subplot(2,n,i+1)
+      img = test_dataset[t_idx[i]][0].unsqueeze(0).to(device)
+      encoder.eval()
+      decoder.eval()
+      with torch.no_grad():
+         rec_img  = decoder(encoder(img))
+      plt.imshow(img.cpu().squeeze().numpy(), cmap='gist_gray')
+      ax.get_xaxis().set_visible(False)
+      ax.get_yaxis().set_visible(False)
+      if i == n//2:
+        ax.set_title('Original images')
+      ax = plt.subplot(2, n, i + 1 + n)
+      plt.imshow(rec_img.cpu().squeeze().numpy(), cmap='gist_gray')
+      ax.get_xaxis().set_visible(False)
+      ax.get_yaxis().set_visible(False)
+      if i == n//2:
+         ax.set_title('Reconstructed images')
+    plt.show()
+
+#%%
+for epoch in range(epochs):
+    train_loss = train_epoch(vae,device, train_loader,optim)
+    val_loss = test_epoch(vae,device, test_loader)
+    print('\n EPOCH {}/{} \t train loss {:.3f} \t val loss {:.3f}'.format(epoch + 1, epochs,train_loss,val_loss))
+    plot_ae_outputs(vae.encoder,vae.decoder,n=10)
